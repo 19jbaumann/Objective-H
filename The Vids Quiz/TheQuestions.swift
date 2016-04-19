cc317c5ab2d49bb4f316a1803286018918dad5d3
Guest User <coder@mainlab1.local>
2016-04-19 11:30:45 -0700

    hi

---
+++ The Vids Quiz/TheQuestions.swift
@@ -0,0 +1,20 @@
+//
+//  TheQuestions.swift
+//  The Vids Quiz
+//
+//  Created by Coder on 4/19/16.
+//  Copyright © 2016 Objective-Hue. All rights reserved.
+//
+
+import GameKit
+
+struct GameQuestions {
+    
+    let questions = [" Inhow did Alduin die?", "How do you stop the moon from destroying the planet in Majora's Mask"]
+    
+    func getRandomFact() -> String {
+        let randomNumber = GKRandomSource.sharedRandom().nextIntWithUpperBound(questions.count)
+        
+        return questions[randomNumber]
+    }
+}
